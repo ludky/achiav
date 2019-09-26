4bd0cdb7b675fabe680df5e4a75119cfc81f774b
aclifford3 <alec.clifford@gmail.com>
2019-09-26 15:48:34 -0400

    Added icons to bottom tab navigator

--- App.js
+++ App.js
@@ -5,22 +5,38 @@ import Profile from "./components/ProfileComponent";
 import Social from "./components/SocialComponent";
 import Quests from "./components/QuestsComponent";
 import Rewards from "./components/RewardsComponent";
+import {Icon} from "react-native-elements";
 
 const MainNavigator = createBottomTabNavigator(
     {
-        Rewards: Rewards,
+        Rewards: {
+            screen: Rewards,
+            headerTitle: 'Test',
+            navigationOptions: {
+                tabBarIcon: <Icon name='star' type='font-awesome' color='gold'/>
+            }
+        },
         Quests: {
-            screen: Quests
+            screen: Quests,
+            navigationOptions: {
+                tabBarIcon: <Icon name='map' type='font-awesome' color='black'/>
+            }
         },
         Social: {
-            screen: Social
+            screen: Social,
+            navigationOptions: {
+                tabBarIcon: <Icon name='users' type='font-awesome' color='black'/>
+            }
         },
         Profile: {
-            screen: Profile
+            screen: Profile,
+            navigationOptions: {
+                tabBarIcon: <Icon name='user' type='font-awesome'/>
+            }
         }
     },
     {
-      defaultNavigationOptions: {
+      navigationOptions: {
         headerStyle: {
           backgroundColor: '#ba86f7'
         },
